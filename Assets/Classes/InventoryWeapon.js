a4285ac117d413d48b8d0eae727e8ccb9d039d2c
symentax4797 <symsudormrf@gmail.com>
2012-09-05 22:06:42 -0400

    storegivesammo

--- Assets/Classes/InventoryWeapon.js
+++ Assets/Classes/InventoryWeapon.js
@@ -3,12 +3,14 @@
 class InventoryWeapon {
 
 	var weapon : Transform;
+	var weaponName : String;
 	var reloadTimer : ReloadTimer;
 	var powerupTimer : PowerupTimer;
 	var ammoCounter : AmmoCounter;
 
-	function InventoryWeapon(w : Transform) {
+	function InventoryWeapon(w : Transform, name : String) {
 		weapon = w;
+		weaponName = name;
 		reloadTimer = new ReloadTimer(weapon.GetComponent(frisbeeMotion).reloadTime);
 		powerupTimer = new PowerupTimer(weapon.GetComponent(frisbeeMotion).frisbeePowerupTime);
 		ammoCounter = new AmmoCounter(weapon.GetComponent(frisbeeMotion).startingAmmo);
--- Assets/Scripts/loadout.js
+++ Assets/Scripts/loadout.js
@@ -6,10 +6,16 @@ var weapons : Array;
 function Awake() {
 	weapons = new Array();
 	for (var i : int = 0; i < frisbees.Length; i++) {
-		weapons.Add(new InventoryWeapon(frisbees[i]));
+		weapons.Add(new InventoryWeapon(frisbees[i],frisbeeNameExtract(frisbees[i].ToString())));
 	}
 }
 
 function hasWeaponIndex(i : int) {
 	return i < weapons.length;
+}
+
+//omg regular expression
+function frisbeeNameExtract(raw:String){
+	var parts = raw.Split("("[0]);
+	return parts[0];
 }
